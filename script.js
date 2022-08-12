a1380e6109e1face348fbbeced5530ce70a0d2ad
Gabriel Viana <gabrielviana.fcb9@gmail.com>
2022-08-12 20:17:37 -0300

    Mostrando carrinho sem dados

--- script.js
+++ script.js
@@ -104,6 +104,21 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
             qt:modalQt
         });
     }
-
+s
+    updateCart();
     closeModal();
 });
+
+function updateCart() {
+    if(cart.length > 0) {
+        c('aside').classList.add('show');
+
+        for(let i in cart) {
+            let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+
+        }
+
+    } else {
+        c('aside').classList.remove('show');
+    }
+}
