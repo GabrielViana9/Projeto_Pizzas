c67024e6bb9fbb507973877bdbd123ca2feb7978
Gabriel Viana <gabrielviana.fcb9@gmail.com>
2022-08-23 20:28:14 -0300

    Adicionando dados ao carrinho

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ c('.pizzaInfo--addButton').addEventListener('click', ()=>{
             qt:modalQt
         });
     }
-s
+
     updateCart();
     closeModal();
 });
@@ -112,10 +112,32 @@ s
 function updateCart() {
     if(cart.length > 0) {
         c('aside').classList.add('show');
+        c('.cart').innerHTML = '';
 
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            let cartItem = c('.models .cart--item').cloneNode(true);
 
+            let pizzaSizeName;
+            switch(cart[i].size) {
+                case 0:
+                    pizzaSizeName = 'P';
+                    break;
+                case 1:
+                    pizzaSizeName = 'M';
+                    break;
+                case 2:
+                    pizzaSizeName = 'G';
+                    break;
+            }
+
+            let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+
+            cartItem.querySelector('img').src = pizzaItem.img;
+            cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
+            cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+
+            c('.cart').append(cartItem);
         }
 
     } else {
