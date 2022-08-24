3d732883ba2d21db29bd462cce7388e2bd547548
Gabriel Viana <gabrielviana.fcb9@gmail.com>
2022-08-23 21:18:12 -0300

    Finalizando carrinho para PC

--- script.js
+++ script.js
@@ -114,12 +114,19 @@ function updateCart() {
         c('aside').classList.add('show');
         c('.cart').innerHTML = '';
 
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item)=>item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+
+           
             let cartItem = c('.models .cart--item').cloneNode(true);
 
             let pizzaSizeName;
-            switch(cart[i].size) {
+            switch(cart[i].size){
                 case 0:
                     pizzaSizeName = 'P';
                     break;
@@ -130,17 +137,39 @@ function updateCart() {
                     pizzaSizeName = 'G';
                     break;
             }
-
             let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
 
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
-
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', ()=>{
+                if (cart[i].qt > 1){
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+                updateCart();
+            });
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', ()=>{
+                cart[i].qt++;
+                updateCart();
+            });
             c('.cart').append(cartItem);
         }
 
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        c('.subtotal span:last-child').innerHTML = `${subtotal.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})}`;
+        c('.desconto span:last-child').innerHTML = `${desconto.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})}`;
+        c('.total span:last-child').innerHTML = `${total.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})}`;
+
     } else {
         c('aside').classList.remove('show');
-    }
+    };
+};
+function cartCleaner(){
+    updateCart()
+        cart.length = 0;
+    updateCart();
 }
